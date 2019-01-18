fa538a278a77e3408597f69bd4030903f1e4979a
PCWallace <pratyushchhibber@gmail.com>
2019-01-18 20:52:11 +0530

    Add files via upload

---
+++ wallyprimeseive.java
@@ -0,0 +1,209 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+ 
+ 
+/**
+ * Using FastReader and Output Class
+ * @author Wallace
+ */
+ class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        FastReader in = new FastReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Prime  solver = new Prime();
+        solver.solve(1, in, out);
+        out.close();
+    }
+    static class  Prime
+	{
+		public void solve(int testNumber, FastReader in, PrintWriter out)			//main code
+			{
+				
+				boolean[] isprime=new boolean[100000001];	//seive
+				Arrays.fill(isprime,true);
+				isprime[0]=false;
+				isprime[1]=false;
+				for(int i=2;i*i<=100000000;i++)
+					{
+						if(isprime[i])
+							{
+								for(int j=i*i;j<=100000000;j+=i)
+									{
+										isprime[j]=false;
+									}
+							}
+					}
+				int q=in.nextInt();	//number of querries
+				while(q!=0)
+					{
+						int n=in.nextInt();
+						out.println(isprime[n]);
+						q--;
+					}
+			}
+		
+		
+	}
+
+ //for the fastreader
+    static class FastReader {
+        private InputStream stream;
+        private byte[] buf = new byte[8192];
+        private int curChar;
+        private int pnumChars;
+ 
+        public FastReader(InputStream stream) {
+            this.stream = stream;
+        }
+ 
+        private int pread() {
+            if (pnumChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= pnumChars) {
+                curChar = 0;
+                try {
+                    pnumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (pnumChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+ 
+        public int nextInt() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            int res = 0;
+            do {
+                if (c == ',') {
+                    c = pread();
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+	public String next() {
+            return nextString();
+        }
+	
+	 public String nextString() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+	
+	public long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+            return array;
+        }
+	
+	public double[] nextDoubleArray(int n) {
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextDouble();
+            }
+            return array;
+        }
+ 
+	public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+ 
+        public long nextLong() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+	 public double nextDouble() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            double res = 0.0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+      
+ 
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+ 
+    }
+ 
+ 
+} 
