03f8c0d4e22acbb0e870b9cd2bf88c02a6270c5e
PCWallace <pratyushchhibber@gmail.com>
2019-01-20 10:19:08 +0530

    Add files via upload

---
+++ wallykmp.java
@@ -0,0 +1,217 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+ import java.util.*;
+ 
+/**
+ * Using FastReader and Output Class
+ * @author Wallace
+ */
+ class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        FastReader in = new FastReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        KMP  solver = new KMP();
+        solver.solve(1, in, out);
+        out.close();
+    }
+ static class  KMP
+     {
+	public void solve(int testNumber, FastReader in, PrintWriter out)            //main code
+            {
+	    	String s=in.nextString();
+		String t=in.nextString();
+		String big=s+"#"+t;
+		int pi[]=prefix(big);
+		int n=s.length();
+		ArrayList<Integer> al=new ArrayList<>();//stores the indexes from where the pattern is matching
+		int l=big.length();
+		for(int i=n+1;i<l;i++)
+			{
+				if(pi[i]==n)
+					al.add(i-2*n);
+			}
+		int ss=al.size();
+		for(int i=0;i<ss;i++)
+			out.println(al.get(i));	
+            }
+	
+	//prefix function of the string which is being used in various algo
+	public static int[] prefix(String s)
+		{
+			int n=s.length();
+			int pi[]=new int[n];
+			pi[0]=0;
+			for(int i=1;i<n;i++)
+				{
+					int j=pi[i-1];
+					while(j>0 && s.charAt(i)!=s.charAt(j))
+						{
+							j=pi[j-1];
+						}
+					if(s.charAt(i)==s.charAt(j))
+						j++;
+					pi[i]=j;
+				}
+			return pi;
+		}
+	}  
+                
+    
+ //fastreader
+    static class FastReader {
+        private InputStream stream;
+        private byte[] buf = new byte[8192];
+        private int curChar;
+        private int pnumChars;
+ 
+        public FastReader(InputStream stream) {
+            this.stream = stream;
+        }
+ 
+        private int pread() {
+            if (pnumChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= pnumChars) {
+                curChar = 0;
+                try {
+                    pnumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (pnumChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+ 
+        public int nextInt() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            int res = 0;
+            do {
+                if (c == ',') {
+                    c = pread();
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+    public String next() {
+            return nextString();
+        }
+    
+     public String nextString() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+    
+    public long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+            return array;
+        }
+    
+    public double[] nextDoubleArray(int n) {
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextDouble();
+            }
+            return array;
+        }
+ 
+    public int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+ 
+        public long nextLong() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+     public double nextDouble() {
+            int c = pread();
+            while (isSpaceChar(c))
+                c = pread();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = pread();
+            }
+            double res = 0.0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = pread();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+ 
+      
+ 
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+ 
+    }
+ 
+ 
+} 
